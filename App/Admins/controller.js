c353d057f8acf3d190912ea53ac650671ab8492d
Furqan Mohsin <furqanmohsin1@gmail.com>
2021-10-20 14:57:28 +0500

    Deploying

--- App/Admins/controller.js
+++ App/Admins/controller.js
@@ -1,9 +1,9 @@
 const AdminUser = require('./model');
 const jwt = require('jsonwebtoken');
 
-const Users = require('../Users/model');
-const Agents = require('../Agents/model');
-const Products = require('../Products/model');
+// const Users = require('../Users/model');
+// const Agents = require('../Agents/model');
+// const Products = require('../Products/model');
 
 module.exports = {
     Create: async (req, res) => {
@@ -81,7 +81,7 @@ module.exports = {
     Update: async (req, res) => {
         try {
             const id = req.params.id;
-            adminuser = await AdminUser.findOneAndUpdate({_id: id}, {
+            adminuser = await AdminUser.updateOne({_id: id}, {
                 $set: req.body
             });
             return res.status(200).json({
@@ -126,26 +126,26 @@ module.exports = {
             });
         }
     },
-    Dashboard: async (req, res) => {
-        try {
-            const users = await Users.find({});
-            const agents = await Agents.find({});
-            const products = await Products.find({});
-            return res.status(200).json({
-                status: 'Successful',
-                data: {
-                    users: users,
-                    agents: agents,
-                    products: products
-                }
-            })
-        } catch (error) {
-            return res.status(500).json({
-                status: 'Error',
-                message: error.message
-            });
-        }
-    }
+    // Dashboard: async (req, res) => {
+    //     try {
+    //         const users = await Users.find({});
+    //         const agents = await Agents.find({});
+    //         const products = await Products.find({});
+    //         return res.status(200).json({
+    //             status: 'Successful',
+    //             data: {
+    //                 users: users,
+    //                 agents: agents,
+    //                 products: products
+    //             }
+    //         })
+    //     } catch (error) {
+    //         return res.status(500).json({
+    //             status: 'Error',
+    //             message: error.message
+    //         });
+    //     }
+    // }
 }
 
 
